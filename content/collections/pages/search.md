1dececc31cff63166b5376e276616e7c5916b076
fxpopp <felix.c.popp@googlemail.com>
2024-03-24 15:21:26 +0100

    Initial commit

---
+++ content/collections/pages/search.md
@@ -0,0 +1,9 @@
+---
+id: 1ba59997-8364-4fd6-8123-47e1f010fd93
+blueprint: pages
+title: Suchergebnisse
+template: search
+updated_by: 2d1285c3-270b-4f15-8e49-6df64f76dafd
+updated_at: 1711189651
+author: 2d1285c3-270b-4f15-8e49-6df64f76dafd
+---
---
+++ resources/blueprints/vendor/statamic/cms/src/Http/Controllers/OAuthController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Statamic\Http\Controllers;
+
+use Illuminate\Support\Facades\Auth;
+use Laravel\Socialite\Facades\Socialite;
+use Laravel\Socialite\Two\InvalidStateException;
+use Statamic\Facades\OAuth;
+
+class OAuthController
+{
+    public function redirectToProvider($provider)
+    {
+        return Socialite::driver($provider)->redirect();
+    }
+
+    public function handleProviderCallback($provider)
+    {
+        $oauth = OAuth::provider($provider);
+
+        try {
+            $providerUser = $oauth->getSocialiteUser();
+        } catch (InvalidStateException $e) {
+            return $this->redirectToProvider($provider);
+        }
+
+        $user = $oauth->findOrCreateUser($providerUser);
+
+        session()->put('oauth-provider', $provider);
+
+        Auth::login($user, config('statamic.oauth.remember_me', true));
+
+        return redirect()->to($this->successRedirectUrl());
+    }
+
+    protected function successRedirectUrl()
+    {
+        $default = '/';
+
+        $previous = session('_previous.url');
+
+        if (! $query = array_get(parse_url($previous), 'query')) {
+            return $default;
+        }
+
+        parse_str($query, $query);
+
+        return array_get($query, 'redirect', $default);
+    }
+}
